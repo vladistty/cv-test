e81aef4a970fa8efe1bddf0e422efd79205421ad
Vladislav Rudenia <vlrudoffl@gmail.com>
2023-01-31 19:26:56 +0300

    burger menu ready

---
+++ js/burger-menu.js
@@ -0,0 +1,5 @@
+function burgerOnClick() {
+    document.querySelector(".menu__bars").classList.toggle("swap");
+    document.querySelector(".menu__nav").classList.toggle("swap");
+    document.querySelector(".header__menuBack").classList.toggle("header__menuBack_swap");
+  }
